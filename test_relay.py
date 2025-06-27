c689820da3c54e6b7a8edb2ce5e4c030dc3994a1
jettnicolette <jettnicolette6@gmail.com>
2025-06-26 23:00:39 -0400

    Relay/pump on/off

--- test_relay.py
+++ test_relay.py
@@ -1,1 +1,26 @@
 
+import RPi.GPIO as GPIO
+import time
+
+RELAY_PIN = 17  # Update if using a different GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(RELAY_PIN, GPIO.OUT)
+
+try:
+    while True:
+        cmd = input("Type 'on', 'off', or 'exit': ").strip().lower()
+        if cmd == "on":
+            GPIO.output(RELAY_PIN, GPIO.HIGH)
+            print("Relay ON")
+        elif cmd == "off":
+            GPIO.output(RELAY_PIN, GPIO.LOW)
+            print("Relay OFF")
+        elif cmd == "exit":
+            GPIO.cleanup()
+            print("Exiting.")
+            break
+        else:
+            print("Unknown command.")
+except KeyboardInterrupt:
+    GPIO.cleanup()
